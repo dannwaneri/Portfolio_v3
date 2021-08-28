27f908a514bc8b78e2c057c956ebfd6f0dd16b8d
Penuelnwaneri <penuelchudi@gmail.com>
2021-08-28 11:46:06 +0100

    initial project using create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h2>Hello world</h2>
+    </div>
+    <div className="bg"></div>
+    </>
+  );
+}
+
+export default App;
