9186d5e83762d49f7fb906cf5bf8965bcae39aa1
Penuelnwaneri <penuelchudi@gmail.com>
2021-08-29 17:45:53 +0100

    implement header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,26 @@ function App() {
   return (
     <>
     <div className="App">
-      <h2>Hello world</h2>
+      <header className="header">
+        <div className="logo">
+          <span className=""></span>
+        </div>
+        <div className="content">
+          <div className="inner">
+              <h1>Dimension</h1>
+              <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br/>
+						for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+        </div>
+        </div>
+        <nav className="use-middle">
+							<ul>
+								<li><a href="#intro">Intro</a></li>
+								<li><a href="#work">Work</a></li>
+								<li className="is-middle"><a href="#about">About</a></li>
+								<li><a href="#contact">Contact</a></li>
+							</ul>
+						</nav>
+      </header>
     </div>
     <div className="bg"></div>
     </>
